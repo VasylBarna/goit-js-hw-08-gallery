f1ec1fe279130e3afe514112c49463a6e182e6dc
VasylBarna <vasylbarna@gmail.com>
2021-04-22 12:05:24 +0300

    dz8 repair

--- script.js
+++ script.js
@@ -1,78 +1,87 @@
 import gallery from "./gallery-items.js";
 
-const refs = {
-  gallery: document.querySelector(".js-gallery"),
-  lightBox: document.querySelector(".js-lightbox"),
-  lightBoxImg: document.querySelector(".lightbox__image"),
-  lightBoxBtn: document.querySelector('[data-action="close-lightbox"]'),
-};
+const galleryBox = document.querySelector(".js-gallery");
+const lightBox = document.querySelector(".js-lightbox");
+const lightBoxImg = document.querySelector(".lightbox__image");
+const lightBoxBtn = document.querySelector('[data-action="close-lightbox"]');
 
-let currentIndex = 0;
-
-function createImgMarkup(images) {
-  return images.map((img) => {
+function createImgMarkup(gallery) {
+  return gallery.map(({ preview, original, description }) => {
     return `<li class="gallery__item">
   <a
     class="gallery__link"
-    href="${img.original}"
+    href="${original}"
   >
     <img
       class="gallery__image"
-      src="${img.preview}"
-      data-source="${img.original}"
-      alt="${img.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </li>`;
   });
 }
 
-refs.gallery.insertAdjacentHTML("beforeend", createImgMarkup(gallery).join(""));
-refs.gallery.addEventListener("click", onImgClick);
+galleryBox.insertAdjacentHTML("beforeend", createImgMarkup(gallery).join(""));
+galleryBox.addEventListener("click", onImgClick);
 
 function onImgClick(event) {
   event.preventDefault();
   if (!event.target.classList.contains("gallery__image")) {
     return;
   }
-  refs.lightBox.classList.add("is-open");
-  refs.lightBoxImg.src = event.target.dataset.source;
-  createImgMarkup(gallery).forEach((el, index) => {
-    if (el.includes(event.target.src)) {
-      currentIndex = index;
-      document.addEventListener("keydown", imgMove);
-    }
-  });
+
+  lightBox.classList.add("is-open");
+  getImageAttribute(event.target.dataset.source, event.target.alt);
+  window.addEventListener("keydown", onArrowKeyPress);
+  window.addEventListener("keydown", onEscKeyPress);
 }
-function imgMove(event) {
-  if (event.key == "ArrowLeft") {
-    if (currentIndex > 0) {
-      refs.lightBoxImg.src = gallery[currentIndex - 1].original;
-      currentIndex -= 1;
-      console.log(currentIndex);
-    } else {
-      currentIndex = gallery.length;
-    }
-  } else if (event.key == "ArrowRight") {
-    if (currentIndex < gallery.length - 1) {
-      refs.lightBoxImg.src = gallery[currentIndex + 1].original;
-      currentIndex += 1;
-      console.log(currentIndex);
-    } else {
-      currentIndex = -1;
-    }
-  }
+
+function getImageAttribute(src, alt) {
+  lightBoxImg.src = src;
+  lightBoxImg.alt = alt;
 }
-refs.lightBox.addEventListener("click", (event) => {
-  if (event.target.nodeName == "IMG") {
+
+function onArrowKeyPress(event) {
+  let currentIndex = gallery.findIndex((g) => g.original === lightBoxImg.src);
+
+  if (currentIndex < 0) {
     return;
   }
-  refs.lightBox.classList.remove("is-open");
-  refs.lightBoxImg.src = "";
-});
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    refs.lightBox.classList.remove("is-open");
-    refs.lightBoxImg.src = "";
+
+  if (event.code === "ArrowLeft") {
+    currentIndex -= 1;
+    if (currentIndex === -1) {
+      currentIndex = gallery.length - 1;
+    }
+    console.log(currentIndex);
+  } else if (event.code === "ArrowRight") {
+    currentIndex += 1;
+    console.log(currentIndex);
+    if (currentIndex === gallery.length) {
+      currentIndex = 0;
+    }
   }
-});
+
+  getImageAttribute(
+    gallery[currentIndex].original,
+    gallery[currentIndex].description
+  );
+}
+
+const closeBtn = document.querySelector('[data-action="close-lightbox"]');
+closeBtn.addEventListener("click", closeModal);
+lightBoxBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  window.removeEventListener("keydown", onArrowKeyPress);
+  lightBox.classList.remove("is-open");
+  getImageAttribute("", "");
+}
+function onEscKeyPress(event) {
+  if (event.code === "Escape") {
+    closeModal();
+  }
+}
